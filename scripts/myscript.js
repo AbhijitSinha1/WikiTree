097e2102c606bfd41f5223e1c54b0d8b8d2ecf5d
AbhijitSinha <abhijitsinha@abhijitsinha-Studio.(none)>
2013-07-18 20:04:54 +0530

    Made a minor change in the myscript.js.

--- scripts/myscript.js
+++ scripts/myscript.js
@@ -5,7 +5,7 @@ $(document).ready(function(){
 	var this_count=addToStorage(this_url);
 	curr_url=this_url;
 	$("#mw-panel").css("overflow-x","auto");
-	var histPanel="<div id='bodyContent' class='history' style='cursor:pointer'>History</div><ul  style='list-style-type: none;list-style-image: none;' id='bodyContent' class='histList'>"+listStorage("")+"</ul><div class='deleteHistory' id='bodyContent' style='cursor:pointer'>Delete History</div>";
+	var histPanel="<div id='bodyContent' class='history' style='cursor:pointer'>History</div><ul  style='list-style-type: none;list-style-image: none;' id='bodyContent' class='histList'>"+listStorage('')+"</ul><div class='deleteHistory' id='bodyContent' style='cursor:pointer'>Delete History</div>";
 	$("#mw-panel").append(histPanel);
 	$("a").click(function(){
 		insertToStorage(this_count,this);
@@ -25,45 +25,48 @@ $(document).ready(function(){
 	});
 });
 function isCorrectURL(url){
-	if((""+url).split("=").length>1){
+	if((''+url).split('=').length>1){
+		return(0);
+	}
+	if((''+url).split("#").length>1){
 		return(0);
 	}
 	return(1);
 }
 function insertToStorage(this_count,url){
 	if(isCorrectURL(url)){
-		if(localStorage.getItem(""+url)==null){
+		if(localStorage.getItem(''+url)==null){
 			var next_count=1;
-			while(localStorage.getItem(""+this_count+""+next_count)!=null){
+			while(localStorage.getItem(''+this_count+''+next_count)!=null){
 				next_count++;
 			}
-			localStorage.setItem(""+this_count+""+next_count,""+url);
-			localStorage.setItem(""+url,""+this_count+""+next_count);
+			localStorage.setItem(''+this_count+''+next_count,''+url);
+			localStorage.setItem(''+url,''+this_count+''+next_count);
 		}
 	}
 }
 function addToStorage(this_url){
 	var this_count=1;
 	if(isCorrectURL(this_url)){
-		if(localStorage.getItem(""+this_url)==null){
-			while(localStorage.getItem(""+this_count)!=null){
+		if(localStorage.getItem(''+this_url)==null){
+			while(localStorage.getItem(''+this_count)!=null){
 				this_count++;
 			}
-			localStorage.setItem(""+this_count,""+this_url);
-			localStorage.setItem(""+this_url,""+this_count);
+			localStorage.setItem(''+this_count,''+this_url);
+			localStorage.setItem(''+this_url,''+this_count);
 		}
 		else{
-			this_count=localStorage.getItem(""+this_url);
+			this_count=localStorage.getItem(''+this_url);
 		}
 	}
 	return(this_count);
 }
 function listStorage(prefix){
 	var this_count=1;
-	var retStr="";
+	var retStr='';
 	while(localStorage.getItem(prefix+this_count)!=null){
 		var this_url=localStorage.getItem(prefix+this_count);
-		var style="";
+		var style='';
 		if(this_url==curr_url){
 			style="> ";
 		}
